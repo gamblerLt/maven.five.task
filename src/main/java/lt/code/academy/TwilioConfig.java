fcfc0b8b821109a63980e1bf44d6c3597d5fa07e
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-17 15:55:34 +0200

    sms

--- src/main/java/lt/code/academy/TwilioConfig.java
+++ src/main/java/lt/code/academy/TwilioConfig.java
@@ -11,7 +11,7 @@ public class TwilioConfig {
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = new Message.creator(
-                new com.twilio.type.PhoneNumber("+14805318340"),
+                new com.twilio.type.PhoneNumber("+"),
                 "Zinutes tekstas")
                 .create;
         System.out.println(message.getSid);
