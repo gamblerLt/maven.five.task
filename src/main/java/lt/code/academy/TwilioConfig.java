27934ae3321b2219b4223da8f8c83d537d403722
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-17 15:55:04 +0200

    sms

--- src/main/java/lt/code/academy/TwilioConfig.java
+++ src/main/java/lt/code/academy/TwilioConfig.java
@@ -6,8 +6,7 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class TwilioConfig {
-    public static final String ACCOUNT_SID = System.getenv("AC76d91d3bb22a85cc8c16be7cf4e31c15");
-    public static final String AUTH_TOKEN = System.getenv("8e0fbeccc9f07a61e8fa7d9a168c2cca");
+
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
